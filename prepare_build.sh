c8a8d4398b5b8ee5541335ff229d87e3503029da
razarshakyan <razmik.arshakyann@gmail.com>
2025-04-03 14:42:40 +0200

    Update prepare_build.sh

--- prepare_build.sh
+++ prepare_build.sh
@@ -3,7 +3,7 @@
 version=$(rustc --version | awk '{print $2}')
 required_version="1.81.0"
 
-if printf "%s\n%s" "$required_version" "$version" | sort -V | head -n1 | grep -r "$required_version"; then
+if [ "$(printf "%s\n%s" "$required_version" "$version" | sort -V | head -n1)" = "$required_version" ]; then
     echo "rustc is up to date."
 else
     if ! command -v rustup &> /dev/null
